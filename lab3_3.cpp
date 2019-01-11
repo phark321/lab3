f72ebe155bb4797a52a592d0950f2798d333dc8a
lab302-2 <lab302-2@labeng.local>
2019-01-11 17:33:59 +0700

    verion1

---
+++ lab3_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double sum = 0,x =2;
+    while(x<=69){
+        sum += (1/x);
+        x++;
+
+    }
+    cout << sum;
+    
+    return 0;
+}
